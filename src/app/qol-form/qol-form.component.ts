8aaafc400b7cd6c9a99d90150af828436b5cd137
Your Name <youremail@example.com>
2024-06-06 12:45:15 +0200

    Theoretically final

--- src/app/qol-form/qol-form.component.ts
+++ src/app/qol-form/qol-form.component.ts
@@ -13,7 +13,6 @@ import { GreenSpaceService } from '../Green-Space/greenspace.service';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UnemploymentService } from '../Unemployment/unemployment.service';
-
 @Component({
   selector: 'app-qol-form',
   templateUrl: './qol-form.component.html',
@@ -24,6 +23,8 @@ export class QolFormComponent {
     address: '',
     district: '',
     status: '',
+    latitude: '',
+    longitude: ''
   };
   qualityOfLife: number = 0;
   showResults: boolean = false;
@@ -60,7 +61,10 @@ export class QolFormComponent {
   distancePoints: number = 1;
   parkPoints: number = 1;
   greenSpacePoints: number = 1;
-
+  
+  incomeObservable :number=1;
+  homelessnessObservable: number=1;
+  
   constructor(
     private geocodeService: GeocodeService,
     private csvReaderService: CsvReaderService,
@@ -77,7 +81,6 @@ export class QolFormComponent {
   toggleAverageVisibility(visible: boolean) { // So that average is only visible on hover
     this.showAverage = visible;
   }
-
   onSubmit() {
     if (this.showSliders == false) {
       this.calculateInitialModifiers();
@@ -91,14 +94,49 @@ export class QolFormComponent {
       this.AddModifiersTogether();
     }
 
+
+
+
+
+    if (this.qolData.latitude && this.qolData.longitude) {
+      const coordinates = {
+        lat: parseFloat(this.qolData.latitude),
+        lng: parseFloat(this.qolData.longitude)
+      };
+      this.processCoordinates(coordinates);
+    } else {
+      this.geocodeService.getCoordinates(this.qolData.address).pipe(
+        catchError((error) => {
+          alert('Geocoding failed. Please check your internet connection or the address entered.');
+          console.error('Geocoding error:', error);
+          return of(null);
+        })
+      ).subscribe({
+        next: (response) => {
+          if (response?.status === 'OK') {
+            const coordinates = response.results[0].geometry.location;
+            if (coordinates.lat == "") {
+              console.error("Coordinates not available for this address");
+            }
+            this.processCoordinates(coordinates);
+          } else {
+            alert('We cannot find this specific address. Please use an address close to you on a larger street to get similar results or input your coordinates manually. You can find your coordinates here: https://www.latlong.net/convert-address-to-lat-long.html');
+          }
+        },
+        error: (error) => console.error('An error occurred:', error)
+      });
+    }
+  }
+
+  processCoordinates(coordinates: { lat: number, lng: number }) {
     var districtCode: string;
     if (+this.qolData.district < 10) {
       districtCode = `90${this.qolData.district.padStart(3, '')}00`;
     } else {
       districtCode = `9${this.qolData.district.padStart(3, '')}00`;
     }
-    console.log(districtCode);
 
+    console.log("lat: ", coordinates.lat, "lng: ", coordinates.lng);
     const incomeObservable = this.csvReaderService.getDataForDistrict(districtCode, '/assets/Income.csv').pipe(
       catchError((error) => {
         console.error('Income CSV file error:', error);
@@ -112,116 +150,96 @@ export class QolFormComponent {
         return of(null);
       })
     );
-
-    this.geocodeService.getCoordinates(this.qolData.address).pipe(
+    const policeStationsObservable = this.policeStationsService.readCsv('/assets/Police_Locations.csv').pipe(
+      map(stations => stations.filter(station =>
+        this.policeStationsService.calculateDistance(coordinates.lat, coordinates.lng, station.latitude, station.longitude) <= 1000
+      )),
       catchError((error) => {
-        alert('Geocoding failed. Please check your internet connection or the address entered.');
-        console.error('Geocoding error:', error);
-        return of(null);
+        console.error('Police Locations CSV file error:', error);
+        return of([]);
       })
-    ).subscribe({
-      next: (response) => {
-        if (response?.status === 'OK') {
-          const coordinates = response.results[0].geometry.location;
+    );
 
-          if (coordinates.lat == "") {
-            console.error("Coordinates are not available for this address");
-          }
-          console.log("lat: ");
-          const policeStationsObservable = this.policeStationsService.readCsv('/assets/Police_Locations.csv').pipe(
-            map(stations => stations.filter(station =>
-              this.policeStationsService.calculateDistance(coordinates.lat, coordinates.lng, station.latitude, station.longitude) <= 1000
-            )),
-            catchError((error) => {
-              console.error('Police Locations CSV file error:', error);
-              return of([]);
-            })
-          );
+    const hospitalsObservable = this.hospitalService.readCsv('/assets/Hospital.csv').pipe(
+      map(hospitals => this.hospitalService.findNearbyHospitals(hospitals, coordinates.lat, coordinates.lng)),
+      catchError((error) => {
+        console.error('Hospital CSV file error:', error);
+        return of([]);
+      })
+    );
 
-          const hospitalsObservable = this.hospitalService.readCsv('/assets/Hospital.csv').pipe(
-            map(hospitals => this.hospitalService.findNearbyHospitals(hospitals, coordinates.lat, coordinates.lng)),
-            catchError((error) => {
-              console.error('Hospital CSV file error:', error);
-              return of([]);
-            })
-          );
+    const schoolsObservable = this.schoolsService.findSchoolsNear({ lat: coordinates.lat, lng: coordinates.lng }, 500, '/assets/School_Location.csv').pipe(
+      catchError((error) => {
+        console.error('School Location CSV file error:', error);
+        return of([]);
+      })
+    );
 
-          const schoolsObservable = this.schoolsService.findSchoolsNear({ lat: coordinates.lat, lng: coordinates.lng }, 500, '/assets/School_Location.csv').pipe(
-            catchError((error) => {
-              console.error('School Location CSV file error:', error);
-              return of([]);
-            })
-          );
+    const distanceToCenterObservable = new Observable<number>((subscriber) => {
+      const distance = this.centerDistanceService.calculateDistanceFromCenter(coordinates.lat, coordinates.lng);
+      subscriber.next(distance);
+      subscriber.complete();
+    });
 
-          const distanceToCenterObservable = new Observable<number>((subscriber) => {
-            const distance = this.centerDistanceService.calculateDistanceFromCenter(coordinates.lat, coordinates.lng);
-            subscriber.next(distance);
-            subscriber.complete();
-          });
+    const greenspacesObservable = this.greenSpaceService.readCsv('/assets/GreenSpace.csv').pipe(
+      map(greenspaces => this.greenSpaceService.findNearbyGreenSpaces(greenspaces, coordinates.lat, coordinates.lng)),
+      catchError((error) => {
+        console.error('GreenSpace CSV file error:', error);
+        return of([]);
+      })
+    );
 
-          const greenspacesObservable = this.greenSpaceService.readCsv('/assets/GreenSpace.csv').pipe(
-            map(greenspaces => this.greenSpaceService.findNearbyGreenSpaces(greenspaces, coordinates.lat, coordinates.lng)),
-            catchError((error) => {
-              console.error('GreenSpace CSV file error:', error);
-              return of([]);
-            })
-          );
+ 
+    forkJoin([
+      incomeObservable,
+      homelessnessObservable,
+      policeStationsObservable,
+      schoolsObservable,
+      distanceToCenterObservable,
+      hospitalsObservable,
+      greenspacesObservable
+    ]).subscribe({
+      next: ([incomeData, unemploymentData, policeData, schoolsData, centerDistance, hospitalData, greenSpaceData]) => {
+        this.incomeData = incomeData?.totalIncome;
+        this.unemploymentData = unemploymentData;
+        this.policeStationsCount = policeData.length;
+        this.schoolsCount = schoolsData.length;
+        this.distanceToCenter = Math.round(centerDistance);
+        this.hospitalCount = hospitalData.length;
+        this.greenSpaceCount = greenSpaceData.length;
 
-          forkJoin([
-            incomeObservable,
-            homelessnessObservable,
-            policeStationsObservable,
-            schoolsObservable,
-            distanceToCenterObservable,
-            hospitalsObservable,
-            greenspacesObservable
-          ]).subscribe({
-            next: ([incomeData, unemploymentData, policeData, schoolsData, centerDistance, hospitalData, greenSpaceData]) => {
-              this.incomeData = incomeData?.totalIncome;
-              this.unemploymentData = unemploymentData;
-              this.policeStationsCount = policeData.length;
-              this.schoolsCount = schoolsData.length;
-              this.distanceToCenter = Math.round(centerDistance);
-              this.hospitalCount = hospitalData.length;
-              this.greenSpaceCount = greenSpaceData.length;
+        this.incomePoints = this.calculatePoints(this.incomeData, 25000, 37250, 20866);
+        this.unemploymentPoints = this.calculatePoints(this.unemploymentData, 89.91, 123, 32, true);
+        this.policePoints = this.calculatePoints(this.policeStationsCount, 2.1, 11, 0);
+        this.schoolPoints = this.calculatePoints(this.schoolsCount, 3.77, 16, 0);
+        this.hospitalPoints = this.calculatePoints(this.hospitalCount, 2.29, 9, 0);
+        this.greenSpacePoints = this.calculatePoints(this.greenSpaceCount, 90.96, 183, 23);
 
-              this.incomePoints = this.calculatePoints(this.incomeData, 25000, 37250, 20866);
-              this.unemploymentPoints = this.calculatePoints(this.unemploymentData, 89.91, 123, 32, true);
-              this.policePoints = this.calculatePoints(this.policeStationsCount, 2.1, 11, 0);
-              this.schoolPoints = this.calculatePoints(this.schoolsCount, 3.77, 16, 0);
-              this.hospitalPoints = this.calculatePoints(this.hospitalCount, 2.29, 9, 0);
-              this.greenSpacePoints = this.calculatePoints(this.greenSpaceCount, 90.96, 183, 23);
-
-              if (this.distanceToCenter < 1500) {
-                this.distancePoints = 10;
-              } else if (this.distanceToCenter < 3000) {
-                this.distancePoints = 8;
-              } else if (this.distanceToCenter < 4500) {
-                this.distancePoints = 5;
-              } else if (this.distanceToCenter < 6000) {
-                this.distancePoints = 2;
-              } else {
-                this.distancePoints = 0;
-              }
-
-              console.log("Points:  ", "Hospital: ", this.hospitalPoints, "  Police: ", this.policePoints, "  School: ",
-                this.schoolPoints, "  Income: ", this.incomePoints, "  Unemployment: ", this.unemploymentPoints, "  Distance: ",
-                this.distancePoints, "  Green Space: ", this.greenSpacePoints);
-
-              this.qualityOfLife = 0;
-
-              this.AddModifiersTogether();
-              this.calculateQualityOfLife();
-
-              this.showResults = true;
-              this.showSliders = true;
-
-            },
-            error: (error) => console.error('Error in combined Observables', error)
-          });
+        if (this.distanceToCenter < 1500) {
+          this.distancePoints = 10;
+        } else if (this.distanceToCenter < 3000) {
+          this.distancePoints = 8;
+        } else if (this.distanceToCenter < 4500) {
+          this.distancePoints = 5;
+        } else if (this.distanceToCenter < 6000) {
+          this.distancePoints = 2;
+        } else {
+          this.distancePoints = 0;
         }
+
+        console.log("Points:  ", "Hospital: ", this.hospitalPoints, "  Police: ", this.policePoints, "  School: ",
+          this.schoolPoints, "  Income: ", this.incomePoints, "  Unemployment: ", this.unemploymentPoints, "  Distance: ",
+          this.distancePoints, "  Green Space: ", this.greenSpacePoints);
+
+        this.qualityOfLife = 0;
+
+        this.AddModifiersTogether();
+        this.calculateQualityOfLife();
+
+        this.showResults = true;
+        this.showSliders = true;
       },
-      error: (error) => console.error('An error occurred:', error)
+      error: (error) => console.error('Error in combined Observables', error)
     });
   }
 
@@ -291,16 +309,23 @@ export class QolFormComponent {
       + this.greenSpaceModifier * this.greenSpacePoints
     ) / this.allModifiers) * 10) / 10;
   }
+calculatePoints(value: number, avg: number, max: number, min: number, reverse: boolean = false): number {
+    const range = max - min;
 
-  calculatePoints(value: number, avg: number, max: number, min: number, reverse: boolean = false): number {
-    const range = max - min; // Define the range of the data
     let normalizedValue = (value - min) / range;
+
     if (reverse) {
-      normalizedValue = 1 - normalizedValue; // Reverse the scale for negative impacts
+        normalizedValue = 1 - normalizedValue;
     }
-    let points = (normalizedValue * 10);
+
+    const distanceFromAvg = Math.abs(value - avg) ;
+    const normalizedDistanceFromAvg = distanceFromAvg / range;
+
+    let points = (normalizedValue * 7) + (3 - (normalizedDistanceFromAvg * 3));
+
+    points = Math.min(Math.max(points, 1), 10);
 
     console.log("avg ", avg, "   ", normalizedValue, "  points:  ", points);
-    return Math.min(Math.max(points, -10), 35); // Clamp values to stay within -10 and +35
-  }
+    return points;
+}
 }
